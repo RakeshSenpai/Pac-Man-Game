fac2586d6f44cdd7b174aaf534293f84b00f54fe
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-01 23:34:27 +0530

    baord

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const columnCount = 19;
 const tileSize = 32;
 const boardWidth = columnCount*tileSize;
 const boardHeight = rowCount*tileSize;
+let context;
+
+window.onload = function(){
+    board = document.getElementById("board")
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d")
+}

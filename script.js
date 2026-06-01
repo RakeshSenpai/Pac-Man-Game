720735e7a7291a186e7123b8d2150b665f3d8697
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-01 23:52:43 +0530

    images

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ window.onload = function(){
     board.width = boardWidth;
     context = board.getContext("2d")
 }
+
+function laodImages(){
+    wallImage = new Image();
+    wallImage.src = './assets/image/wall (1).png';
+
+    blueGhostImage = new Image();
+    blueGhostImage.src = './assets/image/blueGhost.png';
+    orrangeGhostImage = new Image();
+    orrangeGhostImage.src = './assets/image/orrangeGhostImage';
+    pinkGhostImage = new Image();
+    pinkGhostImage.src = './assets/image/pinkGhost.png';
+    redGhostImage = new Image();
+    redGhostImage.src = './assets/image/redGhost.png';
+
+    
+    pacmanUpImage = new Image();
+    pacmanUpImage.src = './assets/image/pacmanUp.png';
+    pacmanDownImage = new Image();
+    pacmanDownImage.src = './assets/image/pacmanDown.png';
+    pacmanLeftImage = new Image();
+    pacmanLeftImage.src = './assets/image/pacmanLeft.png';
+    pacmanRightImage = new Image();
+    pacmanRightImage.src = './assets/image/pacmanRight.png';
+
+}

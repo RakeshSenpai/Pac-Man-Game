9c0df6d4197538d3fb0e746df44eace2e9e82422
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-09 23:41:10 +0530

    ghost-moving

--- script.js
+++ script.js
@@ -190,11 +190,16 @@ function move(){
     }
 
     for(let ghost of ghosts.values()){
+
+        if(ghost.y == tileSize*9 && ghost.direction != 'U' && ghost.direction != 'D'){
+            ghost.updateDirection('U')
+        }
+
         ghost.x += ghost.velocityX;
         ghost.y += ghost.velocityY;
 
         for(let wall of walls.values()){
-            if(collision(ghost, wall)){
+            if(collision(ghost, wall) || ghost.x <= 0 || ghost.x + ghost.width >= boardWidth){
                 ghost.x -= ghost.velocityX;
                 ghost.y -= ghost.velocityY;
                 const newDirection = directions[Math.floor(Math.random() * 4)];
